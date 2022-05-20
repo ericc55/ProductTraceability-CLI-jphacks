7c23fb9d5c023496c1622b93203b06608ae4cfac
okita-haruru <okitaharuru@gmail.com>
2022-05-20 23:22:19 +0800

    delete login

--- src/router/index.js
+++ src/router/index.js
@@ -172,36 +172,29 @@ const routes = [
         component: () => import(/* webpackChunkName: "403" */ '../views/403.vue'),
       },
     ],
-  }, {
-    path: '/login',
-    name: 'Login',
-    meta: {
-      title: '登录',
-    },
-    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue'),
   },
 ];
 
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-});
+// const router = createRouter({
+//   history: createWebHashHistory(),
+//   routes,
+// });
 
-router.beforeEach((to, from, next) => {
-  document.title = `${to.meta.title} | vue-manage-system`;
-  const role = localStorage.getItem('ms_username');
-  if (!role && to.path !== '/login') {
-    next('/login');
-  } else if (to.meta.permission) {
-    // 如果是管理员权限则可进入，这里只是简单的模拟管理员权限而已
-    if (role === 'admin') {
-      next();
-    } else {
-      next('/403');
-    }
-  } else {
-    next();
-  }
-});
+// router.beforeEach((to, from, next) => {
+//   document.title = `${to.meta.title} | vue-manage-system`;
+//   const role = localStorage.getItem('ms_username');
+//   if (!role && to.path !== '/login') {
+//     next('/login');
+//   } else if (to.meta.permission) {
+//     // 如果是管理员权限则可进入，这里只是简单的模拟管理员权限而已
+//     if (role === 'admin') {
+//       next();
+//     } else {
+//       next('/403');
+//     }
+//   } else {
+//     next();
+//   }
+// });
 
 export default router;
