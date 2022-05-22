71a30ee41f953170da64fe711790d35f4f1b97e9
okita-haruru <okitaharuru@gmail.com>
2022-05-22 16:48:37 +0800

    make metamask not necessary

--- src/utils/web3-core.js
+++ src/utils/web3-core.js
@@ -5,16 +5,16 @@ let web3Default;
 let web3Chain;
 
 const getWeb3 = async () => {
-  window.ethereum.autoRefreshOnNetworkChange = false;
-  if (typeof window.ethereum !== 'undefined') {
-    // myConsole.log('using window.ethereum');
-    // web3Default = new Web3(window.ethereum);
-    web3Chain = new Web3(
-      new Web3.providers.HttpProvider(process.env.VUE_APP_CHAIN),
-    );
-  } else {
-    throw new Error('No wallet installed');
-  }
+  // window.ethereum.autoRefreshOnNetworkChange = false;
+  // if (typeof window.ethereum !== 'undefined') {
+  //   // myConsole.log('using window.ethereum');
+  //   // web3Default = new Web3(window.ethereum);
+  web3Chain = new Web3(
+    new Web3.providers.HttpProvider(process.env.VUE_APP_CHAIN),
+  );
+  // } else {
+  //   throw new Error('No wallet installed');
+  // }
   updateWeb3(web3Chain);
   return {
     web3Default,
